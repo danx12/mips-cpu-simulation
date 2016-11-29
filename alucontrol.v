2a35c7b004e0b26acc9921c51e1bba57224b5222
Christian Macias <chrismacias37@gmail.com>
2016-11-28 20:09:21 -0700

    modified:   alucontrol.v

--- alucontrol.v
+++ alucontrol.v
@@ -4,7 +4,7 @@ module alucontrol(aluop,fun,aluctrl);
 	input wire [5:0] fun;
 	output wire [3:0] aluctrl;
 
-	assign aluctrl[3] = aluop[0] ~& aluOp[0];
+	assign aluctrl[3] = aluop[0] ~& aluop[0];
 	assign aluctrl[2] = aluop[0] | (aluop[1] & fun[1]);
 	assign aluctrl[1] =  ~(aluop[1]) | ~(fun[2]);
 	assign aluctrl[0] =  (aluop[1]) & ((fun[3])|(fun[0]));
