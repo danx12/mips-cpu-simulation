a85cf15f839a428f25096993513107e2e6af2119
Daniel Villarreal <danie.villarreal12@gmail.com>
2016-11-29 17:05:30 -0700

    The declaration for memoryFile was wrong. The sizes of array and registers were switched.

--- memory.v
+++ memory.v
@@ -1,6 +1,6 @@
 module memory(input wire [6:0] readMem, input wire [6:0] writeMem, input wire [31:0] writeData, input wire trigWrite, input wire trigRead, output reg [31:0] readData);
 
-	reg [127:0] memoryFile [31:0];
+	reg [31:0] memoryFile [127:0];
 
 	always @ (posedge trigRead) begin
 		if(trigWrite == 0) begin
