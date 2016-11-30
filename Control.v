1b7d9e30adad0e3a40991f08edcb34d629f8ac5c
Soto53 <tonysoto915@gmail.com>
2016-11-29 20:26:26 -0700

    Controlpath for lab4 and a testbench for it

---
+++ Control.v
@@ -0,0 +1,108 @@
+module ControlPath(
+
+instrWord,
+RegDest,
+RegWrite,
+ALUSrc,
+ALUOp1,
+ALUOp0,
+MemRead,
+MemWrite,
+MemToReg,
+Branch
+
+);
+
+//Input
+input wire[31:0] instrWord; // input being seen by module is 32 bits
+
+
+
+//Output
+
+output reg RegDest;    
+output reg RegWrite; 
+output reg ALUSrc;
+output reg ALUOp1;
+output reg ALUOp0;
+output reg MemRead;
+output reg MemWrite;
+output reg MemToReg;
+output reg Branch;
+
+
+
+always @ (instrWord)
+
+
+
+
+begin
+    // Rformat
+	if(instrWord[31:26] == 6'b000000 )begin   // opcode for r format
+	
+
+	RegDest = 1;
+	RegWrite = 1;
+	ALUSrc = 0;
+	ALUOp1 = 1;
+	ALUOp0 = 0;
+	MemRead = 0;
+	MemWrite = 0;
+	MemToReg = 0;
+	Branch = 0;
+	
+	
+	end
+	
+
+	
+	//lw
+	if(instrWord[31:26] == 6'b100011 )begin //opcode for lw
+	
+	
+	RegDest = 0;
+	ALUSrc = 1;
+	MemToReg = 1;	
+	RegWrite = 1;
+	MemRead = 1;
+	MemWrite = 0;
+	Branch = 0;	
+	ALUOp1 = 0;
+	ALUOp0 = 0;
+	
+		
+
+
+
+	
+	
+	end
+	
+	//sw
+	if(instrWord[31:26] == 6'b101011)begin // opcode for sw
+	
+	RegDest = 1'bx;    // x
+	ALUSrc = 1;
+	MemToReg = 1'bx;	//x
+	RegWrite = 0;
+	MemRead = 0;
+	MemWrite = 1;
+	Branch = 0;	
+	ALUOp1 = 0;
+	ALUOp0 = 0;
+	
+
+
+	
+	end
+	
+	end
+	
+	endmodule
+	
+	
+
+	
+	
+
---
+++ CtrlTB.v
@@ -0,0 +1,78 @@
+module testbench;
+	
+//Input
+output reg[31:0] instrWord; // since the Instruction being seen from the input is the full 32 bit instruction you will send a 32 bit instruction to the input
+
+
+
+//Output
+
+input wire RegDest;    
+input wire RegWrite; 
+input wire ALUSrc;
+input wire ALUOp1;
+input wire ALUOp0;
+input wire MemRead;
+input wire MemWrite;
+input wire MemToReg;
+input wire Branch;
+
+ 
+
+
+	ControlPath controlpath(
+
+instrWord,
+RegDest,
+RegWrite,
+ALUSrc,
+ALUOp1,
+ALUOp0,
+MemRead,
+MemWrite,
+MemToReg,
+Branch
+
+);
+
+
+	
+
+	
+	initial
+	begin
+		// Test Vector 1
+ 		#10
+ 		instrWord = 32'b00000001010101010101010101010101;
+		#10
+		$display("Rformat");
+	    $display("RegDest = %d , \nALUSrc = %d, \nMemToReg = %d, \nRegWrite = %d, \nMemRead = %d,  \nMemWrite = %d, \nBranch  = %d , \nALUOp1 = %d, \nALUOp0 = %d", RegDest, ALUSrc, MemToReg, RegWrite, MemRead, MemWrite, Branch, ALUOp1, ALUOp0);
+		
+  		#10
+ 		instrWord = 32'b10001110101010101010101010101010;
+		#10
+		$display("lw");
+		$display("RegDest = %d , \nALUSrc = %d, \nMemToReg = %d, \nRegWrite = %d, \nMemRead = %d,  \nMemWrite = %d, \nBranch  = %d , \nALUOp1 = %d, \nALUOp0 = %d", RegDest, ALUSrc, MemToReg, RegWrite, MemRead, MemWrite, Branch, ALUOp1, ALUOp0);
+ 		#10
+ 		instrWord = 32'b10101100000000000000000000000000;
+		#10
+		$display("sw");	
+		$display("RegDest = %d , \nALUSrc = %d, \nMemToReg = %d, \nRegWrite = %d, \nMemRead = %d,  \nMemWrite = %d, \nBranch  = %d , \nALUOp1 = %d, \nALUOp0 = %d", RegDest, ALUSrc, MemToReg, RegWrite, MemRead, MemWrite, Branch, ALUOp1, ALUOp0);
+		
+		#10
+ 		instrWord = 32'b11111111111111111111111111111111;
+		#10
+	$display("no op");
+ 		#10
+ 		instrWord = 32'b10010100000000000000000000000000;
+		#10
+	$display("no op");
+ 		#10
+ 		instrWord = 32'b11010100000000000000000000000000;
+		#10
+	$display("no op");
+		
+		$finish;
+ 		
+ 		end
+ 	endmodule
