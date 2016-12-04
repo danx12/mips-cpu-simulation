efc3a91f6e07b11bebaaab50d28e4d56a58b7f39
Daniel Villarreal <danie.villarreal12@gmail.com>
2016-12-03 21:03:30 -0700

    Added location 0,1,2 into memory. Like flashing.

--- memory.v
+++ memory.v
@@ -1,6 +1,14 @@
 module memory(input wire [6:0] address, input wire [31:0] writeData, input wire trigWrite, input wire trigRead, output reg [31:0] readData);
 
 	reg [31:0] memoryFile [127:0];
+	
+	
+	//This are flashed always in memory.
+	initial begin
+	memoryFile[0] = 10;
+	memoryFile[1] = 22;
+	memoryFile[2] = 6;
+	end
 
 	always @ (posedge trigRead) begin
 		if(trigWrite == 0) begin
